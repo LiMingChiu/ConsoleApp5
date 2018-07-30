35911ef16d1328f384016ca4b3962b1ee04a71ae
LeoChiuOuO <40887938+LeoChiuOuO@users.noreply.github.com>
2018-07-30 12:29:44 +0800

    加入專案檔案。

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5566);
+            Console.WriteLine(5566);
+            Console.WriteLine(556677889977);
+            Console.WriteLine(556677889977);
+        }
+    }
+}
